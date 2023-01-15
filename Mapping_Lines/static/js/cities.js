d96640775bc0c5e6b930d86d9b893e45491bdbc8
gasconma1960 <112348240+gasconma1960@users.noreply.github.com>
2023-01-15 00:21:59 -0500

    UPDATE

--- Mapping_Lines/static/js/cities.js
+++ Mapping_Lines/static/js/cities.js
@@ -33,29 +33,3 @@ let cities = [{
   population: 1660272
 }
 ];
-// We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    accessToken: API_KEY
-});
-// Loop through the cities array and create one marker for each city.
-cities.forEach(function(city) {
-  console.log(city)
-  L.marker(city.location).addTo(map);
-});
-// Get data from cities.js
-let cityData = cities;
-// Loop through the cities array and create one marker for each city.
-cityData.forEach(function(city) {
-  console.log(city)
-  L.circleMarker(city.location, {
-     radius: city.population/200000,
-     color: 'orange',
-     background: 'dark',
-        lineweight:4
-     })
-     .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString()+ "</h3>").addTo(map);
- });
-// Then we add our 'graymap' tile layer to the map.
-streets.addTo(map);
